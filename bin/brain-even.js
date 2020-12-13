e8c111c612cbee584eb8f3a8591f89c58267399c
Mansur <mansur98@yandex.ru>
2020-12-13 16:50:29 +0300

    stage_5

---
+++ bin/brain-even.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+/* eslint no-console: "off", import/extensions: "off", no-await-in-loop: "off" */
+import readlineSync from 'readline-sync';
+import { name } from '../src/cli.js';
+
+const getRandNumber = () => Math.floor(Math.random() * 100);
+
+const parityCheck = () => {
+  let correctAnswerCounter = 0;
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
+  for (let i = 1; i <= 3; i++) {
+    const randNumber = getRandNumber();
+    console.log(`Question: ${randNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === 'yes') {
+      switch (randNumber % 2) {
+        case 1:
+          console.log('"yes" is wrong answer ;(. Correct answer was "no"');
+          break;
+        case 0:
+          console.log('Correct!');
+          correctAnswerCounter += 1;
+          break;
+        default:
+      }
+    }
+    if (answer === 'no') {
+      switch (randNumber % 2) {
+        case 1:
+          console.log('Correct!');
+          correctAnswerCounter += 1;
+          break;
+        case 0:
+          console.log('"no" is wrong answer ;(. Correct answer was "yes"');
+          break;
+        default:
+      }
+    }
+    if ((answer !== 'yes') && (answer !== 'no')) {
+      switch (randNumber % 2) {
+        case 1:
+          console.log(`"${answer}" is wrong answer ;(. Correct answer was "no"`);
+          break;
+        case 0:
+          console.log(`"${answer}" is wrong answer ;(. Correct answer was "yes"`);
+          break;
+        default:
+      }
+    }
+  }
+  if (correctAnswerCounter === 3) console.log(`Congratulations, ${name}!`);
+};
+
+parityCheck();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 /* eslint no-console: "off", import/extensions: "off", no-await-in-loop: "off" */
 import readlineSync from 'readline-sync';
 
-const nameFunc = () => {
-  const name = readlineSync.question('May I have your name? ');
-  return console.log(`Hello, ${name}!`);
-};
+export const name = readlineSync.question('May I have your name? ');
+const nameFunc = () => console.log(`Hello, ${name}!`);
 export default nameFunc;
